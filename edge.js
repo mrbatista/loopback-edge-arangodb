571e244c9325ff38ccca06fc3060e430888c829e
Matteo Padovano <mrba7ista@gmail.com>
2015-11-02 18:16:42 +0100

    Reformat code style.

--- edge.js
+++ edge.js
@@ -4,11 +4,11 @@ var _ = require('underscore');
 var debug = require('debug')('loopback:mixins:edge');
 
 module.exports = function(model, options) {
-    debug('Edge mixin for Model %s', model.modelName);
-    options = _.extend({_id: '_id', _from: '_from', _to: '_to', required: true}, options);
-    debug('options', options);
+  debug('Edge mixin for Model %s', model.modelName);
+  options = _.extend({_id: '_id', _from: '_from', _to: '_to', required: true}, options);
+  debug('options', options);
 
-    model.defineProperty(options._id, {type: String});
-    model.defineProperty(options._from, {type: String, required: options.required});
-    model.defineProperty(options._to, {type: String, required: options.required});
+  model.defineProperty(options._id, {type: String});
+  model.defineProperty(options._from, {type: String, required: options.required});
+  model.defineProperty(options._to, {type: String, required: options.required});
 };
